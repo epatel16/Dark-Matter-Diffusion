5288e877394f441de28d1e491d335c17ed693c10
Aditi Chandrashekar <aditijc@gmail.com>
2024-11-01 15:46:47 -0700

    update train w/ new normalization

--- src/train.py
+++ src/train.py
@@ -12,30 +12,32 @@ from diffusers import UNet2DModel
 
 from tqdm import tqdm
 from datasets import SlicedDataset, NPYDataset
-from utils import LogTransform
-from sample import sample
+from utils import sample, sample_v1, get_constants
 from accelerate import Accelerator
+from torch.optim.lr_scheduler import CosineAnnealingLR
 
 from datetime import datetime
 import os
 import argparse
+import pdb
 
-from models import DarkMatterUNet
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="A script to parse training parameters.")
     
     # Adding arguments
     parser.add_argument("--dataset", default="IllustrisTNG", type=str, required=False, help="IllustrisTNG, Astrid, or SIMBA.")
-    parser.add_argument("--epochs", type=int, required=False, default= 30, help="Number of epochs for training.")
-    parser.add_argument("--batch_size", type=int, required=False, default=32, help="Batch size for training.")
-    parser.add_argument("--learning_rate", type=float, default=4e-4,required=False, help="Learning rate for training.")
+    parser.add_argument("--epochs", type=int, required=False, default= 20, help="Number of epochs for training.")
+    parser.add_argument("--batch_size", type=int, required=False, default=12, help="Batch size for training.")
+    parser.add_argument("--learning_rate", type=float, default=1e-4,required=False, help="Learning rate for training.")
     parser.add_argument("--img_size", type=int, required=False, default=64, help="Image size. Single int, (H = W).")
     parser.add_argument("--out_path", type=str, required=False, default="/groups/mlprojects/dm_diffusion/model_out/", help="Path to save models.")
     
     # Parsing arguments
     return parser.parse_args()
 
+# TODO: Move to config --> Work w/ 64x64, Test 256x256
 
 def main(args):
     accelerator = Accelerator()
@@ -46,96 +48,99 @@ def main(args):
     os.makedirs(save_path, exist_ok=True)
 
     npy_file = f"/groups/mlprojects/dm_diffusion/data/Maps_Mcdm_{args.dataset}_LH_z=0.00.npy"
+    
+    log_transform = transforms.Lambda(
+        lambda x: torch.log10(x + 1)  # Applying log transformation
+    )
+
+    norm_transform = transforms.Lambda( # Using transform from -1 to 1
+        lambda x: ((x - x.min()) / (x.max() - x.min()) - 0.5)*2
+    )
+
+    constants = get_constants(args.dataset)
+    # Using transforms.Normalize((constants['mean'],), (constants['std'],)) where mean and std correspond 
+    # to the dataset does not work
+
     transform = transforms.Compose([transforms.ToTensor(),
-                                    transforms.RandomCrop((args.img_size, args.img_size)),
-                                    LogTransform(),
-                                    transforms.Normalize((0.0,), (1.0,))])
+                                    transforms.Resize((args.img_size, args.img_size)),
+                                    # transforms.RandomCrop((args.img_size, args.img_size)),
+                                    log_transform,
+                                    norm_transform, 
+                                    # transforms.Normalize((constants['mean'],), (constants['std'],))
+    ])
 
     dataset = SlicedDataset(npy_file, transform=transform)
+    # pdb.set_trace()
+
     dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
-    noise_scheduler = DDPMScheduler(num_train_timesteps=10000)
+    noise_scheduler = DDPMScheduler(num_train_timesteps=1000)
 
-    # Create a model
-    model = UNet2DModel( # TODO: pull this into a config
-        sample_size=args.img_size,  # the target image resolution
-        in_channels=1,  # the number of input channels, 3 for RGB images
-        out_channels=1,  # the number of output channels
-        layers_per_block=2,  # how many ResNet layers to use per UNet block
-        block_out_channels=(64, 128, 256),  # the number of output channels for each UNet block
+    model = UNet2DModel(
+        sample_size=args.img_size,                  
+        in_channels=1,                    
+        out_channels=1,                   
         down_block_types=(
-            "DownBlock2D",  # a regular ResNet downsampling block
-            "DownBlock2D",
-            "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
-        ),
+            "DownBlock2D", "DownBlock2D", "DownBlock2D", "AttnDownBlock2D"
+        ),                                
         up_block_types=(
-            "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
-            "UpBlock2D",
-            "UpBlock2D",
-        ),
+            "AttnUpBlock2D", "UpBlock2D", "UpBlock2D", "UpBlock2D"
+        ),                                
+        block_out_channels=(48, 96, 192, 384),  
+        layers_per_block=2,               
+        norm_num_groups=8,                
+        act_fn="silu",                    
+        # time_embedding_type="fourier",    # Breaks it
+        attention_head_dim=8,             
+        add_attention=True,               
+        num_train_timesteps=1000,         
+        downsample_type='resnet',
+        upsample_type='resnet'
     )
-    # model = DarkMatterUNet(in_channels=1, out_channels=1, base_channels=64)
-
-    # model = UNet2DModel(
-    #     sample_size=256,  # Image resolution
-    #     in_channels=1,  # Number of input channels (e.g., grayscale images)
-    #     out_channels=1,  # Number of output channels
-    #     layers_per_block=2,  # Number of ResNet layers per UNet block
-    #     block_out_channels=(64, 128, 256, 512),  # Channels for each block
-    #     down_block_types=(
-    #         "DownBlock2D",  # Regular downsampling block
-    #         "AttnDownBlock2D",  # Downsampling block with spatial attention
-    #         "DownBlock2D",
-    #         "AttnDownBlock2D",
-    #     ),
-    #     up_block_types=(
-    #         "AttnUpBlock2D",  # Upsampling block with spatial attention
-    #         "UpBlock2D",
-    #         "AttnUpBlock2D",
-    #         "UpBlock2D",
-    #     ),
-    # )
-
+    # Added resnet blocks instead of conv, fourier time embeddings, out channels for blocks
+    
     # Prepare everything with Accelerator
     optimizer = torch.optim.AdamW(model.parameters(), lr=args.learning_rate)
+    scheduler = CosineAnnealingLR(optimizer, T_max=args.epochs, eta_min=1e-6)
     model, optimizer, dataloader = accelerator.prepare(model, optimizer, dataloader)
 
-    # Training loop
+
     losses = []
     for epoch in range(args.epochs):
-        # Add tqdm progress bar for the epoch
+        imgs = True
         with tqdm(dataloader, desc=f"Epoch {epoch+1}/{args.epochs}", unit="batch") as tepoch:
             for step, image in enumerate(tepoch):
+                if imgs: 
+                    plt.figure()
+                    plt.imshow(image[0,0,:,:].detach().cpu().numpy())
+                    plt.savefig(os.path.join(save_path, f'GT_ep{epoch}.png'), format='png')
+                    plt.close()
+                    imgs = False
                 clean_images = image.to(accelerator.device)
                 # Sample noise to add to the images
                 noise = torch.randn(clean_images.shape).to(clean_images.device)
                 bs = clean_images.shape[0]
 
-                # Sample a random timestep for each image
+                # Sample random timestep for each image
                 timesteps = torch.randint(
                     0, noise_scheduler.config.num_train_timesteps, (bs,), device=clean_images.device
                 ).long()
 
                 # Add noise to the clean images according to the noise magnitude at each timestep
                 noisy_images = noise_scheduler.add_noise(clean_images, noise, timesteps)
-                # print('NOISY IMGS: ', noisy_images.shape)
-                # print('CLEAN IMGS: ', clean_images.shape)
-                # Get the model prediction
+
                 noise_pred = model(noisy_images, timesteps)
 
-                # Calculate the loss
-                # print(noise_pred.shape, noise.shape)
                 noise_pred = noise_pred[0] 
                 loss = F.mse_loss(noise_pred, noise)
                 accelerator.backward(loss)
                 losses.append(loss.item())
 
-                # Update the model parameters with the optimizer
                 optimizer.step()
                 optimizer.zero_grad()
 
-                # Update tqdm with the current loss
                 tepoch.set_postfix(loss=loss.item())
 
+        scheduler.step()
         # Calculate and print the loss for the last epoch
         loss_last_epoch = sum(losses[-len(dataloader):]) / len(dataloader)
         print(f"Epoch {epoch+1} loss: {loss_last_epoch}")
@@ -146,12 +151,12 @@ def main(args):
         torch.save(unwrapped_model.state_dict(), model_save_path)
         print(f"Model saved to {model_save_path}")
 
+        # Sample
+        sample_v1(model, noise_scheduler, os.path.join(save_path, f'ep{epoch}_sample.png'), device='cuda')
+
+    # Save all losses
     losses = np.array(losses)
     np.save(f'{save_path}/losses.npy', losses)
-
-    # Generate a sample using the final model
-    unwrapped_model = accelerator.unwrap_model(model)
-    model_sample = sample(unwrapped_model, noise_scheduler, f'{save_path}/final_sample.png')
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -4,58 +4,31 @@ import torchvision
 import matplotlib.pyplot as plt
 
 
-# def sample(model, noise_scheduler, out_path, device='cpu'):
-#   model.eval()
-#   sample = torch.randn(1, 1, 64, 64).to(device)
+def sample_v1(model, noise_scheduler, out_path, device='cpu'):
+  model.eval()
+  sample = torch.randn(1, 1, 64, 64).to(device)
 
-#   store = []
+  store = []
 
-#   for i, t in enumerate(noise_scheduler.timesteps):
-#       print(i, t)
-#       with torch.no_grad():
-#           residual = model(sample, t).sample
+  for i, t in enumerate(noise_scheduler.timesteps):
+      if i % 100 == 0: print(i, t)
+      with torch.no_grad():
+          residual = model(sample, t).sample
 
-#       sample = noise_scheduler.step(residual, t, sample).prev_sample
+      sample = noise_scheduler.step(residual, t, sample).prev_sample
 
-#       if i == 0 or (i + 1) % 100 == 0:
-#         store.append(sample[0])
+      if i == 0 or (i + 1) % 100 == 0:
+        store.append(sample[0])
 
-#   f, axarr = plt.subplots(1,11, figsize=(64, 64))
-#   for i in range(len(store)):
-#     axarr[i].imshow(denormalize(store[i]).cpu().permute(1, 2, 0))
-#     axarr[i].axis("off")
-#   plt.savefig(out_path, format='png')
-#   print(f'Saved at {out_path}!')
+  f, axarr = plt.subplots(1,11, figsize=(64, 64))
+  for i in range(len(store)):
+    im = axarr[i].imshow(store[i].cpu().permute(1, 2, 0))
+    plt.colorbar(im, ax=axarr[i])
+    axarr[i].axis("off")
+    # torch.save(store[i])
+  plt.savefig(out_path, format='png')
+  print(f'Saved at {out_path}!')
 
-# def sample(model, noise_scheduler, out_path, device='cpu'):
-#     model.eval()
-#     sample = torch.randn(1, 1, 64, 64).to(device)
-
-#     store = []
-
-#     for i, t in enumerate(noise_scheduler.timesteps):
-#         print(i, t)
-#         with torch.no_grad():
-#             # Convert t to a tensor and add a batch dimension
-#             t_tensor = torch.tensor([t], dtype=torch.int64).to(device)
-
-#             # Pass the timestep tensor with a batch dimension to the model
-#             residual = model(sample, t_tensor)
-
-#         # Update the sample using the noise scheduler
-#         # Convert the timestep t to an integer before passing to noise_scheduler.step()
-#         sample = noise_scheduler.step(residual, int(t), sample).prev_sample
-
-#         if i == 0 or (i + 1) % 100 == 0:
-#             store.append(sample.cpu().clone())  # Save for visualization
-
-#     # Plot the samples
-#     f, axarr = plt.subplots(1, len(store), figsize=(64, 64))
-#     for i in range(len(store)):
-#         axarr[i].imshow(denormalize(store[i][0]).permute(1, 2, 0))  # Assuming denormalize reshapes properly
-#         axarr[i].axis("off")
-#     plt.savefig(out_path, format='png')
-#     print(f'Saved at {out_path}!')
 
 # Updated sampling function
 def sample(model, noise_scheduler, out_path, device='cpu'):
@@ -65,7 +38,7 @@ def sample(model, noise_scheduler, out_path, device='cpu'):
     store = []
 
     for i, t in enumerate(noise_scheduler.timesteps):
-        if i % 100 == 0: print(i, t)
+        if i % 1000 == 0: print(i, t)
         with torch.no_grad():
             # Convert t to a tensor and add a batch dimension
             t_tensor = torch.tensor([t], dtype=torch.int64).to(device)
@@ -94,13 +67,12 @@ def generate_sample(model_path, model, noise_scheduler, out_path, img_size=64, d
     model.load_state_dict(torch.load(model_path, map_location=device))
 
     # Generate sample
-    sample(model, noise_scheduler, out_path, device=device)
+    sample_v1(model, noise_scheduler, out_path, device=device)
 
+def get_constants(dataset_name='IllustrisTNG'):
+    path = f'/groups/mlprojects/dm_diffusion/data/{dataset_name}_constants.pt'
+    return torch.load(path)
 
-
-class LogTransform:
-  def __call__(self, x):
-      return torch.log(x)  # Adding a small value to avoid log(0)
 
 def denormalize(tensor, mean=10.971004486083984, std=0.5090954303741455): #TODO: make a lookup table, these are params for illustris
   """
@@ -115,3 +87,4 @@ def denormalize(tensor, mean=10.971004486083984, std=0.5090954303741455): #TODO:
       torch.Tensor: The denormalized tensor.
   """
   return tensor * std + mean
+
