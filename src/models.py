bf198382992ff3aa318d022504209946d85ec33a
Aditi Chandrashekar <aditijc@gmail.com>
2024-12-04 23:01:12 -0800

    updated models and frb code

--- src/models.py
+++ src/models.py
@@ -48,6 +48,33 @@ class DiffUNet(nn.Module):
         return self.unet(x, t).sample
 
 
+class DiffUNet_MultiCond(nn.Module):
+    def __init__(self, config, conditional=True):
+        super(DiffUNet_MultiCond, self).__init__()
+        self.unet = UNet2DModel(
+            sample_size=config["image_size"],
+            in_channels=config["conditioning_channels"] + config['target_channels'],
+            out_channels=config['target_channels'],
+            down_block_types=("DownBlock2D", "DownBlock2D", "DownBlock2D", "AttnDownBlock2D"),
+            up_block_types=("AttnUpBlock2D", "UpBlock2D", "UpBlock2D", "UpBlock2D"),
+            block_out_channels=(48, 96, 192, 384),
+            layers_per_block=2,
+            norm_num_groups=8,
+            act_fn="silu",
+            attention_head_dim=8,
+            add_attention=True,
+            downsample_type='resnet',
+            upsample_type='resnet'
+        )
+        self.conditional = conditional
+        self.config = config
+
+    def forward(self, x, t, condition=None):
+        for cond in condition:
+            cond = cond.expand(-1, -1, x.size(2), x.size(3))
+            x = torch.cat((x, cond), dim=1)
+        return self.unet(x, t).sample
+
 class DarkMatterUNet(nn.Module):
     def __init__(self, in_channels=1, out_channels=1, base_channels=64):
         super(DarkMatterUNet, self).__init__()
